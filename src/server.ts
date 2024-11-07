b22a6b0a2291af72ffc3bdccedd017b33cf97b11
Gabs <gabsxaviercel@gmail.com>
2024-11-07 12:52:14 -0300

    Modificação para uso com Vercel

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import routes from './routes';
 
 const app = express();
+const port = process.env.PORT || 3333
 
 app.use(cors())
 app.use(express.json());
@@ -11,4 +12,8 @@ app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-module.exports = app;
+app.listen(port, () => {
+    console.log(`Servidor rodando porta ${port}`);
+});
+
+export default app;
